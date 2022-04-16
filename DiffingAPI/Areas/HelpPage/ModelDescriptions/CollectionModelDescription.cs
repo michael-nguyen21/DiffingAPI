ce4b2333a7b16bab22b759e51dcb78f160973674
Michael Nguyen <michaelnguyen21@outlook.com>
2022-04-16 16:56:12 -0400

    Add project files.

---
+++ DiffingAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace DiffingAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
